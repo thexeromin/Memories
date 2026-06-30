28324f6bee2460d613cc10932575f6dccc8019c4
thexeromin <thexeromin@gmail.com>
2026-06-30 12:31:48 +0530

    refactor: initialize project

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,5 @@
-import { NativeTabs } from "expo-router/unstable-native-tabs";
-import { Colors } from "@/constants/themes";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const colors = Colors["light"];
-
-  return (
-    <NativeTabs
-      backgroundColor={colors.background}
-      indicatorColor={colors.backgroundElement}
-      labelStyle={{ selected: { color: colors.text } }}
-    >
-      <NativeTabs.Trigger name="photos">
-        <NativeTabs.Trigger.Label>Photos</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon sf="photo.fill" md="photo" />
-      </NativeTabs.Trigger>
-    </NativeTabs>
-  );
+  return <Stack />;
 }
