5647eaaee3d453e6ba92f69ddccc7b6b728aa1d2
thexeromin <thexeromin@gmail.com>
2026-07-02 11:38:16 +0530

    fix: android build unmatched route error

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -20,7 +20,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="photos"
+        name="index"
         options={{
           title: "Photos",
           tabBarIcon: ({ color }) => (
